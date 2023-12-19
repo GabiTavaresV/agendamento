84e5e1e9251592e83cdb0c3cae836d3028d98a9b
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-12-19 12:43:36 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("OLA");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.info(`Listening on port ${port}`));
