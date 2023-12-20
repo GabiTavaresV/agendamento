a72755efb771a4b8baeb007cd90df7cb03d78c09
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-12-20 12:33:58 -0300

    feat: add MongoClient

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
+import { router } from "./routes";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  await MongoClient.connect();
+  const app = express();
+  app.use(router);
 
-const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3333;
 
-app.listen(port, () => console.info(`Listening on port ${port}`));
+  app.listen(port, () => console.info(`Listening on port ${port}`));
+};
+
+main();
