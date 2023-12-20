ee7c16708761106c908db7ccbd24b2ca2d5d1835
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-12-20 12:37:04 -0300

    feat: add MongoCreateProfissional

---
+++ src/repositories/mongo-post-profissional.ts
@@ -0,0 +1,27 @@
+import {
+  CreateProfissionalParams,
+  ICreateProfissionalRepository,
+} from "../controllers/professional/protocolsCreate";
+import { MongoClient } from "../database/mongo";
+import { MongoUser } from "../models/mongo-protocols";
+import { Profissional } from "../models/profissional";
+
+export class MongoCreateProfissional implements ICreateProfissionalRepository {
+  async createUser(params: CreateProfissionalParams): Promise<Profissional> {
+    const { insertedId } = await MongoClient.db
+      .collection("profissional")
+      .insertOne(params);
+
+    const user = await MongoClient.db
+      .collection<MongoUser>("test")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw new Error("User not created");
+    }
+
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
+}
