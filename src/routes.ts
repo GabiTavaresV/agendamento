28d3932fbde94be7918199a69b2c1ec6af8dca72
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-12-20 15:08:31 -0300

    feat: add get router

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,20 @@
 import express from "express";
-import { CreateProfessionalController } from "./controllers/professional/createProfessional";
+import { CreateProfessionalController } from "./controllers/create-professional/create-professional";
 import { MongoCreateProfissional } from "./repositories/mongo-post-profissional";
+import { GetProfessionalController } from "./controllers/get-professional/get-professional";
+import { MongoGetUsersRepository } from "./repositories/mongo-get-profissional";
 
 export const router = express.Router();
 
 const mongoCreateProfissionalRepository = new MongoCreateProfissional();
 const createProfissionalController = new CreateProfessionalController(
   mongoCreateProfissionalRepository
+);
+
+const mongoGetProfissional = new MongoGetUsersRepository();
+
+const getProfissionalController = new GetProfessionalController(
+  mongoGetProfissional
 );
 
 router.post("/v1/create-profissional", async (req, res) => {
@@ -17,9 +25,9 @@ router.post("/v1/create-profissional", async (req, res) => {
   res.status(statusCode).send(body);
 });
 
-router.get(
-  "/v1/find-profissional",
-  createProfissionalController.getProfissional
-);
+router.get("/v1/find-profissional", async (req, res) => {
+  const { body, statusCode } = await getProfissionalController.handle();
+  res.status(statusCode).send(body);
+});
 
 router.get("/healthcheck", (req, res) => res.json("OK"));
