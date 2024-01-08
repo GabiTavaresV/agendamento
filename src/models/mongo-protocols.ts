cf1a513fb7e6f478700431a7089f8c9289ce1c90
Gabriela Tavares <gabriela.ttavares@gmail.com>
2024-01-08 11:43:12 -0300

    feat: add MongoService

--- src/models/mongo-protocols.ts
+++ src/models/mongo-protocols.ts
@@ -1,3 +1,5 @@
 import { Profissional } from "./profissional";
+import { Services } from "./services";
 
 export type MongoUser = Omit<Profissional, "id">;
+export type MongoService = Omit<Services, "id">;
