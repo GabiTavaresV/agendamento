38f41254576275c1507360e0a867b65e2458ce60
Gabriela Tavares <gabriela.ttavares@gmail.com>
2024-01-10 18:41:07 -0300

    feat: add create service by profissional router

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,15 @@ import { CreateServicesController } from "./controllers/create-services/create-s
 import { MongoCreateServices } from "./repositories/mongo-post-services";
 import { GetServiceController } from "./controllers/get-service/get-service";
 import { MongoGetServiceRepository } from "./repositories/mongo-get-service";
+import { CreateServiceByProfessionalController } from "./controllers/create-service-by-professional/create-service-by-professional";
+import { MongoCreateServicesByProfissional } from "./repositories/mongo-post-services-profissional";
 
 export const router = express.Router();
 
 const mongoCreateProfissionalRepository = new MongoCreateProfissional();
 const mongoCreateServices = new MongoCreateServices();
+const mongoCreateServicesByProfissional =
+  new MongoCreateServicesByProfissional();
 const createProfissionalController = new CreateProfessionalController(
   mongoCreateProfissionalRepository
 );
@@ -30,6 +34,12 @@ const getServiceController = new GetServiceController(
   mongoGetServiceRepository
 );
 
+const createServiceByProfessionalController =
+  new CreateServiceByProfessionalController(
+    mongoCreateServicesByProfissional,
+    mongoGetServiceRepository
+  );
+
 router.post("/v1/create-profissional", async (req, res) => {
   const { body, statusCode } = await createProfissionalController.handle({
     body: req.body,
@@ -42,6 +52,15 @@ router.post("/v1/create-service", async (req, res) => {
   const { body, statusCode } = await createServicesController.handle({
     body: req.body,
   });
+
+  res.status(statusCode).send(body);
+});
+
+router.post("/v1/create-service-by-profissional", async (req, res) => {
+  const { body, statusCode } =
+    await createServiceByProfessionalController.handle({
+      body: req.body,
+    });
 
   res.status(statusCode).send(body);
 });
